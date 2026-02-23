324a53f5ce268f7f55cee3f504addf195a6cd5df
Tim <tim.gg.yi@gmail.com>
2026-02-23 14:10:03 +0000

    feat: complete CMakeLists.txt, pre-compiled micro-ros-pico-sdk, and FreeRTOS configure file

--- src/firmware/pico_i2c_scan/FreeRTOSConfig.h
+++ src/firmware/pico_i2c_scan/FreeRTOSConfig.h
@@ -1,114 +1,88 @@
 /*
- * FreeRTOS V202212.01
- * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of
- * this software and associated documentation files (the "Software"), to deal in
- * the Software without restriction, including without limitation the rights to
- * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
- * the Software, and to permit persons to whom the Software is furnished to do so.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
- * https://www.FreeRTOS.org
- * https://github.com/FreeRTOS
+ * FreeRTOS Kernel Configuration for BraveHart V2 (RP2040)
+ * Optimized for: Dual-Core SMP, micro-ROS, and BNO085 IMU (50Hz)
  */
 
 #ifndef FREERTOS_CONFIG_H
 #define FREERTOS_CONFIG_H
 
-/* Scheduler settings */
-#define configUSE_EVENT_GROUPS                  1
-#define configUSE_PREEMPTION                    1
+/* ----------------------------------------------------------------      
+ * Scheduler Settings
+ * ---------------------------------------------------------------- */
+#define configUSE_PREEMPTION                    1   // Enable preemption to ensure sensor tasks interrupt low-priority ones
+#define configUSE_TIME_SLICING                  1
 #define configUSE_TICKLESS_IDLE                 0
-#define configCPU_CLOCK_HZ                      125000000
-#define configTICK_RATE_HZ                      ( ( TickType_t ) 1000 )
+#define configCPU_CLOCK_HZ                      125000000 // Default 125MHz
+#define configTICK_RATE_HZ                      ( ( TickType_t ) 1000 ) // 1ms tick, suitable for 50Hz sampling
 #define configMAX_PRIORITIES                    32
-#define configMINIMAL_STACK_SIZE                ( ( uint16_t ) 256 )
+#define configMINIMAL_STACK_SIZE                ( ( uint16_t ) 512 ) // Increase min stack size to prevent micro-ROS overflow
 #define configMAX_TASK_NAME_LEN                 16
-#define configUSE_16_BIT_TICKS                  0
 #define configIDLE_SHOULD_YIELD                 1
-#define configUSE_TASK_NOTIFICATIONS            1
-#define configUSE_MUTEXES                       1
-#define configUSE_RECURSIVE_MUTEXES             1
+#define configUSE_MUTEXES                       1   // Required for micro-ROS
+#define configUSE_RECURSIVE_MUTEXES             1   // Required for I2C locking
 #define configUSE_COUNTING_SEMAPHORES           1
 #define configQUEUE_REGISTRY_SIZE               8
-#define configUSE_QUEUE_SETS                    0
-#define configUSE_TIME_SLICING                  1
-#define configUSE_NEWLIB_REENTRANT              0
-#define configENABLE_BACKWARD_COMPATIBILITY     0
-#define configNUM_THREAD_LOCAL_STORAGE_POINTERS 5
+#define configUSE_TASK_NOTIFICATIONS            1
 
-/* SMP (Symmetric Multi-Processing) specific settings for RP2040 */
-#define configNUMBER_OF_CORES                   2   // Enable Dual Core
-#define configTICK_CORE                         0   // Which core handles the system tick
-#define configRUN_MULTIPLE_PRIORITIES           1   // Allow tasks with different priorities to run at the same time
-#define configUSE_CORE_AFFINITY                 1   // Allow binding tasks to a specific core
+/* ----------------------------------------------------------------
+ * SMP (Symmetric Multi-Processing) Settings
+ * ---------------------------------------------------------------- */
+#define configNUMBER_OF_CORES                   2   // Enable dual-core parallelism
+#define configTICK_CORE                         0   // Core 0 handles system tick
+#define configRUN_MULTIPLE_PRIORITIES           1
+#define configUSE_CORE_AFFINITY                 1   // Enable core affinity to lock IMU to Core 1
 
-/* Memory allocation settings */
+/* ----------------------------------------------------------------
+ * Memory Allocation
+ * ---------------------------------------------------------------- */
 #define configSUPPORT_STATIC_ALLOCATION         0
-#define configSUPPORT_DYNAMIC_ALLOCATION        1
-#define configTOTAL_HEAP_SIZE                   ( ( size_t ) ( 128 * 1024 ) ) // 128KB for heap
+#define configSUPPORT_DYNAMIC_ALLOCATION        1   // micro-ROS requires dynamic memory (Heap 4)
+#define configTOTAL_HEAP_SIZE                   ( ( size_t ) ( 128 * 1024 ) ) // Reserve 128KB for micro-ROS
 
-/* Hook functions */
+/* ----------------------------------------------------------------
+ * Hook Functions & Debugging
+ * ---------------------------------------------------------------- */
 #define configUSE_IDLE_HOOK                     0
 #define configUSE_TICK_HOOK                     0
-#define configCHECK_FOR_STACK_OVERFLOW          2   // High level stack checking
+#define configCHECK_FOR_STACK_OVERFLOW          2   // Enable advanced stack overflow checking
 #define configUSE_MALLOC_FAILED_HOOK            1
-#define configUSE_DAEMON_TASK_STARTUP_HOOK      0
+#define configASSERT( x ) if( ( x ) == 0 ) { taskDISABLE_INTERRUPTS(); for( ;; ); }
 
-/* Run time and task stats settings */
-#define configGENERATE_RUN_TIME_STATS           0
-#define configUSE_TRACE_FACILITY                1
-#define configUSE_STATS_FORMATTING_FUNCTIONS    1
-
-/* Co-routine settings */
-#define configUSE_CO_ROUTINES                   0
-#define configMAX_CO_ROUTINE_PRIORITIES         2
-
-/* Software timer settings */
+/* ----------------------------------------------------------------
+ * Software Timer Settings
+ * ---------------------------------------------------------------- */
 #define configUSE_TIMERS                        1
 #define configTIMER_TASK_PRIORITY               ( configMAX_PRIORITIES - 1 )
 #define configTIMER_QUEUE_LENGTH                10
 #define configTIMER_TASK_STACK_DEPTH            1024
 
-/* Interrupt nesting behaviour configuration. */
-/* RP2040 uses 2 priority bits (0-3), with 0 being highest priority */
+/* ----------------------------------------------------------------
+ * Interrupt Settings
+ * ---------------------------------------------------------------- */
 #define configKERNEL_INTERRUPT_PRIORITY          ( 3 << 6 )
 #define configMAX_SYSCALL_INTERRUPT_PRIORITY     ( 1 << 6 )
 
-/* * Set the following definitions to 1 to include the API function, or zero
- * to exclude the API function. 
- * FIXED: Added "INCLUDE_" prefix to prevent macro substitution of function names.
- */
+/* ----------------------------------------------------------------
+ * Included API Functions
+ * ---------------------------------------------------------------- */
 #define INCLUDE_vTaskPrioritySet                1
 #define INCLUDE_uxTaskPriorityGet               1
 #define INCLUDE_vTaskDelete                     1
-#define INCLUDE_vTaskCleanUpResources           0
 #define INCLUDE_vTaskSuspend                    1
 #define INCLUDE_vTaskDelayUntil                 1
 #define INCLUDE_vTaskDelay                      1
 #define INCLUDE_xTaskGetSchedulerState          1
 #define INCLUDE_xTaskGetCurrentTaskHandle       1
 #define INCLUDE_uxTaskGetStackHighWaterMark     1
-#define INCLUDE_xTaskGetIdleTaskHandle          1
-#define INCLUDE_xTimerCreate                    1
-#define INCLUDE_xTimerIsTimerActive             1
 #define INCLUDE_xTimerStart                     1
 #define INCLUDE_xTimerStop                      1
-#define INCLUDE_xTimerChangePeriod              1
-#define INCLUDE_xTimerDelete                    1
-#define INCLUDE_xTimerReset                     1
-#define INCLUDE_xTimerPendFunctionCall          1
 
-/* Map the FreeRTOS port interrupt handlers to their CMSIS standard names. */
-#define vPortSVCHandler    isr_svcall
-#define xPortPendSVHandler isr_pendsv
-#define xPortSysTickHandler isr_systick
+/* ----------------------------------------------------------------
+ * RP2040 Specific Interrupt Handlers
+ * ---------------------------------------------------------------- */
+// Note: For SMP, these mappings ensure FreeRTOS handles M0+ exceptions
+#define vPortSVCHandler                         vPortSVCHandler
+#define xPortPendSVHandler                      xPortPendSVHandler
+#define xPortSysTickHandler                     xPortSysTickHandler
 
 #endif /* FREERTOS_CONFIG_H */
